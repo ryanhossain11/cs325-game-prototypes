2474e2a407f86fac283c6cf0f323e8b612900738
Ryan Hossain <ryanhossain@Ryan-2.local>
2020-02-06 20:34:21 -0500

    update4

--- HW0/js/main.js
+++ HW0/js/main.js
@@ -25,7 +25,7 @@ function make_main_game_state( game )
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Don't DIE!!.", style );
+        var text = game.add.text( game.world.centerX, 15, "Don't DIE!.", style );
         text.anchor.setTo( 0.5, 0.0 );
     }
 
